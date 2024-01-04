b93f1a0c884662e2d52171cdb922fa269afd25ff
Caleb Mieszko <calebmieszko@gmail.com>
2024-01-03 20:42:12 -0800

    Finished version - no cherries on top yet.

--- index.js
+++ index.js
@@ -25,3 +25,80 @@ How will you add that information to the map?
 - This requires tapping in to the Foursquare API - I've got some research to do!
 
 */
+
+
+
+// get user location                                                         
+async function getCoords(){
+    // set geolocation options
+    const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+    };
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}                   
+
+// get the tiles
+const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+
+// query Foursquare API for locations the user selects
+async function placeSearch(selection, coords) {
+    try {
+        const searchParams = new URLSearchParams({
+          query: selection,
+          ll: coords,
+          open_now: 'true',
+          sort: 'DISTANCE'
+        });
+        const results = await fetch(
+          `https://api.foursquare.com/v3/places/search?${searchParams}`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              Authorization: 'fsq3VdkyX8R8j/VCAEqNrnYWw7YcdSyv9G8JWIbAlwleyp4=',
+            }
+          }
+        );
+        const data = await results.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// generate markers for each location we pass in
+async function createMarkers(locations, map) {
+    locations.forEach(location => {
+        let position = [location.geocodes.main.latitude, location.geocodes.main.longitude]
+        let marker = L.marker(position)
+        marker.addTo(map).bindPopup(`${location.name} ${location.location.address}`).openPopup();
+    }); 
+}
+
+// on load, use the user's location to generate the map and listen for location selections
+window.onload = async () => {
+    const coords = await getCoords()
+    const strCoords = `${coords[0]},${coords[1]}`
+    let map = L.map('map').setView(coords, 15);
+    tiles.addTo(map);
+    userMarker = L.marker(coords)
+    userMarker.addTo(map).bindPopup('Your location').openPopup();
+    // query business type from user when drop down choice is made
+    const selectElement = document.querySelector('#location-select')
+    selectElement.addEventListener('input', async (event)=> {
+        const selection = event.target.value
+        console.log(selection, strCoords)  
+        let result = await placeSearch(selection, strCoords)
+        let locations = result.results
+        console.log(locations)
+        createMarkers(locations, map)
+    })
+}
